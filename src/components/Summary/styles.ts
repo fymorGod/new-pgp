68ece47401ba2b7dba21af4cfec31c35dbb96463
fymorGod <mufinagamer@gmail.com>
2024-11-04 09:06:51 -0300

    feat: set summary styles code

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
 
-export
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
     container: {
         flexDirection: 'column',
         alignItems: 'flex-start',
